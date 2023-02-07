dff0862083aa8e6da0507f1d59771a0d0b65b4e4
PRABHUKUMAR J <nelsonprabhu07088@gmail.com>
2023-02-07 12:04:36 +0530

    new ignore

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
     ini_set("display_errors","0");
     $mode = "local";
     if ($mode == "local") {
-        $dbcon = mysqli_connect("localhost", "root", "", "saloon") or die("Could not connect to Database");
+        $dbcon = mysqli_connect("localhost", "root", "mysql", "saloon") or die("Could not connect to Database");
     }
     if ($mode == "production") {
         $dbcon = mysqli_connect("sql304.epizy.com", "epiz_32968153", "9cLcFv93ECj8qM", "epiz_32968153_saloon") or die("Could not connect to Database");
