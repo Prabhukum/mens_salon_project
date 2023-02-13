3d7759893a4e361097f5cdf094072416227508f3
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2023-02-13 11:49:43 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,21 +3,21 @@
 	session_start();
     ini_set("display_errors","0");
 
-    $mode = "local";
+    $mode = "production";
     global $pdo;
     global $dbcon;
     if ($mode == "local") {
         // Host Name
-        $dbhost = 'localhost';
+        $dbhost = '54.254.31.129:3306';
 
         // Database Name
         $dbname = 'saloon';
 
         // Database Username
-        $dbuser = 'root';
+        $dbuser = 'admin';
 
         // Database Password
-        $dbpass = 'mysql';
+        $dbpass = 'Ajith@123$';
 
         // Defining base url
         define("BASE_URL", "");
@@ -35,15 +35,15 @@
             echo "Connection error :" . $exception->getMessage();
         }
     }
-    if ($mode == "local") {
+    if ($mode == "production") {
         // Host Name
-        $dbhost = 'localhost';
+        $dbhost = '0.0.0.0:3306';
 
         // Database Name
         $dbname = 'saloon';
 
         // Database Username
-        $dbuser = 'root';
+        $dbuser = 'admin';
 
         // Database Password
         $dbpass = 'Ajith@123$';
@@ -54,7 +54,7 @@
         // Getting Admin url
         define("ADMIN_URL", BASE_URL . "admin" . "/");
 
-        $dbcon = mysqli_connect("localhost", "root", "Ajith@123$", "saloon") or die("Could not connect to Database");
+        $dbcon = mysqli_connect($dbhost, $dbuser, $dbpass , $dbname) or die("Could not connect to Database");
    
         try {
             $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
