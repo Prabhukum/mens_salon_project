1a6e9d85a71f7dc151941ab6e17b0f66383af01a
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2023-02-17 13:05:35 +0530

    css path update

--- app/HaircolorCard.php
+++ app/HaircolorCard.php
@@ -8,7 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HairColor</title>
-        <link rel="stylesheet" href="app\haircolor.css">
+        <link rel="stylesheet" href="../includes/css/haircolor.css">
     </head>
     <body>
     <div class="container" style="margin-top:50px;">
