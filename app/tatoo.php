4e88eea7c56082cd5e1c5f7504d85fbc76ad0a1c
PRABHUKUMAR J <nelsonprabhu07088@gmail.com>
2023-03-15 16:10:41 +0530

    1

--- app/tatoo.php
+++ app/tatoo.php
@@ -142,18 +142,31 @@ rel="stylesheet"
     <div class="row mx-auto my-auto">
         <div id="recipeCarousel" class="carousel slide w-100" data-ride="carousel">
             <div class="carousel-inner w-100" role="listbox">
-         
-                <div class="carousel-item active">
+              <?php
+                $statement = $pdo->prepare("SELECT * FROM tbl_product WHERE p_is_featured=? AND p_is_active=?");
+                // $statement = $pdo->prepare("SELECT * FROM tbl_product");
+                $statement->execute(array(1,1));
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);     
+                $i = 0;                         
+                foreach ($result as $row) {
+                  if($i == 0){
+                    echo '<div class="carousel-item active">';
+                    $i++;
+                  } else {
+                    echo '<div class="carousel-item">';
+                  }
+              ?>
                     <div class="col-md-4">
                         <div class="card-image">
-                            <!-- <img class="img-fluid" src="..\app\saloon\<?php echo $row['p_featured_photo']; ?>"> -->
                             <div class="card-image mb-45 text-center">
-                         <div class="img-fluid">    <!-- <a href="#" data-abc="true"> -->
-                            <img class="img-fluid" src="..\app\saloon\mens-long-hairstyles-1.jpg">
-                            </a> <span><i class="fa fa-rupee"></i> 41,000</span>
+                         <div class="img-fluid">
+                              <img class="img-fluid" src="saloon\<?php echo $row['p_featured_photo']; ?>">
+                              <div class="cost-featured">
+                                  <span><i class="fa fa-rupee"></i> 41,000</span>
+                              </div>
                             <div class="product-action">
-                                <div class="product-action-style"> <a href="#"> <i class="fa fa-plus"></i> </a> <a href="#"> <i
-                                    class="fa fa-heart"></i> </a> <a href="#"> <i class="fa fa-shopping-cart"></i> </a>
+                                <div class="product-action-style">
+                                   <a href="#"> <i class="fa fa-plus"></i> </a> <a href="#"> <i class="fa fa-heart"></i> </a> <a href="#"> <i class="fa fa-shopping-cart"></i> </a>
                                 </div>
                             </div>
                           </div>
@@ -162,71 +175,9 @@ rel="stylesheet"
                       </div>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <div class="col-md-4">
-                        <div class="card-image">
-                        <div class="card-image mb-45 text-center">
-                         <div class="img-fluid"> 
-                            <img class="img-fluid" src="..\app\saloon\mens-long-hairstyles-1.jpg">
-                            </a> <span><i class="fa fa-rupee"></i> 41,000</span>
-                            <div class="product-action">
-                                <div class="product-action-style"> <a href="#"> <i class="fa fa-plus"></i> </a> <a href="#"> <i
-                                    class="fa fa-heart"></i> </a> <a href="#"> <i class="fa fa-shopping-cart"></i> </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                        </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-4">
-                        <div class="card-image">
-                            <img class="img-fluid" src="..\app\saloon\short-haircuts-1-3.jpg">
-                            <div class="product-action">
-                                <div class="product-action-style"> <a href="#"> <i class="fa fa-plus"></i> </a> <a href="#"> <i
-                                    class="fa fa-heart"></i> </a> <a href="#"> <i class="fa fa-shopping-cart"></i> </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-4">
-                        <div class="card-image">
-                            <img class="img-fluid" src="..\app\saloon\undercut-styles-6.jpg">
-                            <div class="product-action">
-                                <div class="product-action-style"> <a href="#"> <i class="fa fa-plus"></i> </a> <a href="#"> <i
-                                    class="fa fa-heart"></i> </a> <a href="#"> <i class="fa fa-shopping-cart"></i> </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-4">
-                        <div class="card-image">
-                            <img class="img-fluid" src="..\app\saloon\short-haircuts-1-3.jpg">
-                            <div class="product-action">
-                                <div class="product-action-style"> <a href="#"> <i class="fa fa-plus"></i> </a> <a href="#"> <i
-                                    class="fa fa-heart"></i> </a> <a href="#"> <i class="fa fa-shopping-cart"></i> </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="col-md-4">
-                        <div class="card-image">
-                            <img class="img-fluid" src="..\app\saloon\undercut-styles-6.jpg">
-                            <div class="product-action">
-                                <div class="product-action-style"> <a href="#"> <i class="fa fa-plus"></i> </a> <a href="#"> <i
-                                    class="fa fa-heart"></i> </a> <a href="#"> <i class="fa fa-shopping-cart"></i> </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                  }
+                ?>	
             </div>
             <a class="carousel-control-prev " href="#recipeCarousel" role="button" data-slide="prev">
               <i class="fa fa-angle-left"></i>
@@ -267,15 +218,22 @@ rel="stylesheet"
 <div class="main">
   <h1>Tatoo session</h1>
   <ul class="cards">
+  <?php
+    $statement = $pdo->prepare("SELECT * FROM tbl_product");
+    // $statement = $pdo->prepare("SELECT * FROM tbl_product");
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);      
+    foreach ($result as $row) {
+  ?>
     <li class="cards_item">
       <div class="card">
         <div class="card_image">
-        <img src="..\app\saloon\pexels-dmitry-zvolskiy-1805600.jpg"></div>
+        <img src="saloon/<?php echo $row['p_featured_photo']; ?>"></div>
          <div class="card_content">
-          <h2 class="card_title">Card Grid Layout</h2>
-          <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <p class="item-price strike item-price1 "><strike>₹ 500<?php echo $row['p_old_price']; ?></strike> <b>
-            <?php echo $row['p_current_price']; ?>₹ 55/- </b></p>
+          <h2 class="card_title"><?php echo $row['p_name']; ?></h2>
+          <p class="card_text"><?php echo $row['p_description']; ?></p>
+          <!-- <p class="card_text"><?php echo $row['p_short_description']; ?></p> -->
+          <p class="item-price strike item-price1"><strike>₹<?php echo $row['p_old_price']; ?></strike> <b><?php echo $row['p_current_price']; ?> </b></p>
           <button class="btn-small-ser mt-2" type="button"> Book now </button>
           <a href="#" class="btn-small-ser mt-2">Add to Cart</a>
                          
@@ -283,75 +241,9 @@ rel="stylesheet"
         </div>
       </div>
     </li>
-    <li class="cards_item">
-      <div class="card">
-        <div class="card_image"><img src="..\app\saloon\pexels-dmitry-zvolskiy-1805600.jpg"></div>
-        <div class="card_content">
-          <h2 class="card_title">Card Grid Layout</h2>
-          <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <p class="item-price strike item-price1 "><strike>₹ 500<?php echo $row['p_old_price']; ?></strike> <b>
-            <?php echo $row['p_current_price']; ?>₹ 55/- </b></p>
-          <button class="btn-small-ser mt-2" type="button"> Book now </button>
-          <a href="#" class="btn-small-ser mt-2">Add to Cart</a>
-                         
-        </div>
-      </div>
-    </li>
-    <li class="cards_item">
-      <div class="card">
-        <div class="card_image"><img src="..\app\saloon\pexels-dmitry-zvolskiy-1805600.jpg"></div>
-        <div class="card_content">
-          <h2 class="card_title">Card Grid Layout</h2>
-          <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <p class="item-price strike item-price1 "><strike>₹ 500<?php echo $row['p_old_price']; ?></strike> <b>
-            <?php echo $row['p_current_price']; ?>₹ 55/- </b></p>
-          <button class="btn-small-ser mt-2" type="button"> Book now </button>
-          <a href="#" class="btn-small-ser mt-2">Add to Cart</a>
-                         
-        </div>
-      </div>
-    </li>
-    <li class="cards_item">
-      <div class="card">
-        <div class="card_image"><img src="..\app\saloon\pexels-gabriel-lara-3657562.jpg"></div>
-        <div class="card_content">
-          <h2 class="card_title">Card Grid Layout</h2>
-          <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <p class="item-price strike item-price1 "><strike>₹ 500<?php echo $row['p_old_price']; ?></strike> <b>
-            <?php echo $row['p_current_price']; ?>₹ 55/- </b></p>
-          <button class="btn-small-ser mt-2" type="button"> Book now </button>
-          <a href="#" class="btn-small-ser mt-2">Add to Cart</a>
-        </div>
-      </div>
-    </li>
-    <li class="cards_item">
-      <div class="card">
-        <div class="card_image"><img src="..\app\saloon\pexels-cottonbro-studio-4125587.jpg"></div>
-        <div class="card_content">
-          <h2 class="card_title">Card Grid Layout</h2>
-          <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <p class="item-price strike item-price1 "><strike>₹ 500<?php echo $row['p_old_price']; ?></strike> <b>
-            <?php echo $row['p_current_price']; ?>₹55/- </b></p>
-         
-          <button class="btn-small-ser mt-2" type="button"> Book now </button>
-          <a href="#" class="btn-small-ser mt-2">Add to Cart</a>
-                         
-        </div>
-      </div>
-    </li>
-    <li class="cards_item">
-      <div class="card">
-        <div class="card_image"><img src="..\app\saloon\pexels-dmitry-zvolskiy-1805600.jpg"></div>
-        <div class="card_content">
-          <h2 class="card_title">Card Grid Layout</h2>
-          <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-          <p class="item-price strike item-price1 "><strike>₹ 500<?php echo $row['p_old_price']; ?></strike> <b>
-            <?php echo $row['p_current_price']; ?>₹ 55/- </b></p>
-          <button class="btn-small-ser mt-2" type="button"> Book now </button>
-          <a href="#" class="btn-small-ser mt-2">Add to Cart</a>             
-        </div>
-      </div>
-    </li>
+    <?php
+      }
+    ?>	
   </ul>
 </div>
 </div>  
